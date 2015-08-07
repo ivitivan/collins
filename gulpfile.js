88ee087f4c1d9d6a300610ecbdf64a2de8cdb5e3
ivitivan <ivitivan@gmail.com>
2015-08-07 12:24:01 +0300

    Add error handler to gulpfile
    - Add plumber-gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var dist = 'dist';
 var gulp = require('gulp');
 var jsdoc = require('gulp-jsdoc');
 var babel = require('gulp-babel');
+var plumber = require('gulp-plumber');
 var jsSrc = src + '/**/*.js';
 
 gulp.task('watch', function() {
@@ -17,7 +18,9 @@ gulp.task('jsdoc', function() {
 
 gulp.task('babel', function() {
 	return gulp.src(jsSrc)
+		.pipe(plumber())
 		.pipe(babel())
+		.pipe(plumber.stop())
 		.pipe(gulp.dest(dist));
 });
 
