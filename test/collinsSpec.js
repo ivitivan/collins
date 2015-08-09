f84a30c273d4d9fe37eda8ddce42afc45aa17940
ivitivan <ivitivan@gmail.com>
2015-08-09 16:28:18 +0300

    Add search test
    - Should return an object
    - Should work with optional parameters

--- test/collinsSpec.js
+++ test/collinsSpec.js
@@ -5,16 +5,26 @@ var accessKey = config.ACCESS_KEY;
 
 describe('Collins', function() {
 	var Collins, collins;
+	var dictionaryCode;
+
 	beforeEach(function(done) {
-		Collins = require('../lib/collins.js');
+		Collins = require('../dist/collins.js');
 		collins = new Collins(serverName, accessKey);
 		done();
+	});
+
+	describe.skip('#get_', function() {
+		it('should work with optional parameters', function(done) {
+			collins.get_('/dictionaries');
+			collins.get_('')
+		})
 	});
 	
 	it('should get a list of available dictionaries', function(done) {
 		collins.dictionaries(function(err, data) {
 			if (err) return done(err);
 			expect(data).to.be.a('array');
+			dictionaryCode = data[0].dictionaryCode;
 			done();
 		});
 	});
@@ -23,15 +33,52 @@ describe('Collins', function() {
 		collins.dictionaries(function(err, data) {
 			if (err) return done(err);
 			var dict = data[0];
-			console.log('code', data[0].dictionaryCode);
 			collins.dictionary(data[0].dictionaryCode, function(err, data) {
-				console.log('data', data);
+				debugger;
 				if (err) return done(err);
 				expect(data).to.be.a('object');
-				expect(data).to.equal(dict);
+				expect(data).to.deep.equal(dict);
 				done();
 			});
 		});
-	})
+	});
+
+	describe('#search', function() {
+		beforeEach(function(done) {
+			if (!dictionaryCode) {
+				collins.dictionaries(function(err, data) {
+					if (err) return done(err);
+					expect(data).to.be.a('array');
+					dictionaryCode = 'dictionaryCode', data[0].dictionaryCode;
+					done();
+				});
+			}
+			done();
+		});
+
+		it('should search', function(done) {
+			collins.search(dictionaryCode, 'wood', 10, 1, function(err, data) {
+				if (err) return done(err);
+				expect(data).to.be.a('object');
+				done();
+			});
+		});
+
+		it('shouldnt fall without optional parameters', function(done) {
+			collins.search(dictionaryCode, 'book', function(err, data) {
+				if(err) return done(err);
+				expect(data).to.be.a('object');
+				done();
+			});
+		});
+
+		it('shouldnt fall without optional parameters (1 parameter included)', function(done) {
+			collins.search(dictionaryCode, 'book', 10, function(err, data) {
+				if(err) return done(err);
+				expect(data).to.be.a('object');
+				done();
+			});
+		});
+	});
 
 });
